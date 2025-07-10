f9ebe8a17340b2b8b217287dcc14aac261f249f4
Ahmed Shahriar Sajib <ahmed_shahriar.sajib@smail.th-koeln.de>
2025-07-10 02:08:26 +0200

    source code files

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "app_name" {
+  default = "hr-management"
+}
+
+variable "db_username" {
+  default = "hr_admin"
+}
+
+variable "db_password" {
+  default = "123456789"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "django_secret_key" {
+  default = "123456789"
+}
+
+variable "db_name" {
+  default     = "hr_management"
+}
+
